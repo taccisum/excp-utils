2ab1b547cbe5e76fa873bb8ea761fa9d62585f24
taccisum <liaojinfeng@deepexi.com>
2021-08-10 12:01:46 +0800

    fix: add assertions

--- src/main/java/com/i61/apsara/excp/ExceptionUtilsAutoConfiguration.java
+++ src/main/java/com/i61/apsara/excp/ExceptionUtilsAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.i61.apsara.excp;
 
 import com.i61.apsara.excp.config.ExceptionProperties;
 import com.i61.apsara.excp.remote.DingTalkRobotClientFacade;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,9 +18,19 @@ public class ExceptionUtilsAutoConfiguration {
 
     @Bean
     public DingTalkRobotClientFacade dingTalkRobotClientFacade() {
+        String keys = properties.getDingTalk().getRobot().getKeys();
+        String accessToken = properties.getDingTalk().getRobot().getAccessToken();
+
+        if (StringUtils.isBlank(keys)) {
+            throw new IllegalArgumentException(keys);
+        }
+        if (StringUtils.isBlank(accessToken)) {
+            throw new IllegalArgumentException(accessToken);
+        }
+
         return new DingTalkRobotClientFacade(
-                properties.getDingTalk().getRobot().getKeys(),
-                properties.getDingTalk().getRobot().getAccessToken()
+                keys,
+                accessToken
         );
     }
 }
